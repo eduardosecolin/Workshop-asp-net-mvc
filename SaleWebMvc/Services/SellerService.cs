1bddee19c5567a37e888dcdcb0a93b589ef804d8
Eduardo Secolin <dusecolin@yahoo.com.br>
2018-10-22 19:16:55 -0300

    View de cadastro de vendedores

--- SaleWebMvc/Services/SellerService.cs
+++ SaleWebMvc/Services/SellerService.cs
@@ -16,5 +16,10 @@ namespace SaleWebMvc.Services {
         public List<Seller> FindAll(){
             return _context.Seller.ToList();
         }
+
+        public void Insert(Seller seller){
+            _context.Add(seller);
+            _context.SaveChanges();
+        }
     }
 }
